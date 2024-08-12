92e266a558fd8f373f80df9ee008f8196abf3105
ledarca <ledarca@gmail.com>
2024-08-12 12:31:30 -0500

    Feature: Added a footer component with multiple sections and a copyright notice
    - Implemented the footer component with four main sections: About Us, Policies, Blog, and Social Media.
    - Each section contains relevant links for easy navigation.
    - Added a copyright notice at the bottom of the footer.
    - To be used on all pages as a template

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+<footer>
+                <div className="footer-section">
+                    <h3>About Us</h3>
+                    <ul>
+                        <li><a href="#">Our team</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Location</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h3>Policies</h3>
+                    <ul>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Cookie Policy</a></li>
+                        <li><a href="#">FAQs</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h3>Blog</h3>
+                    <ul>
+                        <li><a href="#">Travel guides</a></li>
+                        <li><a href="#">Popular</a></li>
+                        <li><a href="#">Destination</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h3>Social Media</h3>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                    </ul>
+                </div>
+                <div className="footer-copyright">
+                    <p>&copy; 2024 TravelAmigos. All rights reserved.</p>
+                </div>
+            </footer>
+    </>
+  )
+}
+
+export default Footer
