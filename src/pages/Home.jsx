c825b25056d8793cb325f1f9654e8b3167ea4265
ledarca <ledarca@gmail.com>
2024-08-12 12:45:10 -0500

    feat: Agregar página de inicio con FormSearch y múltiples secciones para Descubrir, Reseñas y Viajes
    - Componente FormSearch integrado en la página de inicio.
    - Se agregaron tres secciones principales: Descubrir, Reseñas y Viajes, pero aquí podemos agregar las secciones que queramos.
    - Cada sección contiene contenido relevante para involucrar a los usuarios con diferentes aspectos de la planificación de viajes.
    
    *** Nota: aunque sigue siendo un componente, prefiero etiquetarlo como una página para poder trabajar más cómodamente, por ejemplo, búsqueda, inicio de sesión, etc.***

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import FormSearch from "../components/FormSearch";
+
+function Home() {
+  return (
+    <>
+        <div>
+            <section className="slider-section">
+                <FormSearch />
+            </section>
+
+            <section className="auto-generated-section">
+                <div className="container">
+                <h2>Discover</h2>
+                <p>Explore new and exciting destinations curated just for you.</p>
+                </div>
+            </section>
+
+            <section className="auto-generated-section">
+                <div className="container">
+                <h2>Reviews</h2>
+                <p>Read what other travelers have to say about their experiences.</p>
+                </div>
+            </section>
+
+            <section className="auto-generated-section">
+                <div className="container">
+                <h2>Trips</h2>
+                <p>Plan your next adventure with our tailored trip packages.</p>
+                </div>
+            </section>
+        </div>
+    </>
+  );
+}
+
+export default Home;
