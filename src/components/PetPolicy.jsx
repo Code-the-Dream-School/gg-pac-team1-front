bd45de4c5711090bb6dc61ab41fcc2b70949324b
ledarca <ledarca@gmail.com>
2024-09-01 08:39:33 -0500

    Add PetPolicy component to display hotel pet policy

---
+++ src/components/PetPolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PetPolicy() {
+  return (
+    <div className="hotel-pet-policy">
+      <h3 className="pet-policy-title">Pet Policy</h3>
+      <p className="pet-policy-description">Pets are allowed on request. Charges may apply.</p>
+    </div>
+  );
+}
+
+export default PetPolicy;
