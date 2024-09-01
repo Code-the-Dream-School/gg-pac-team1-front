ebd4cf2b761611856dbb5b7f518dd51af8e042f3
ledarca <ledarca@gmail.com>
2024-09-01 08:44:13 -0500

    Created PetPolicy component with dynamic content based on hotel props; integrated PetPolicy component into the hotel details page.

--- src/components/PetPolicy.jsx
+++ src/components/PetPolicy.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function PetPolicy() {
+function PetPolicy({ hotel }) {
   return (
-    <div className="hotel-pet-policy">
+    <>
       <h3 className="pet-policy-title">Pet Policy</h3>
       <p className="pet-policy-description">Pets are allowed on request. Charges may apply.</p>
-    </div>
+    </>
   );
 }
 
