66ea28baf55737029c7f5d40d3b4fb487ed045b4
ledarca <ledarca@gmail.com>
2024-09-11 14:05:29 -0500

    Update Modal.jsx to format reservation summary as plain text for backend

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 import './Modal.css'; // Asegúrate de crear un archivo CSS para los estilos del modal
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const Modal = ({ isOpen, onClose, reservationDetails }) => {
   const [userEmail, setUserEmail] = useState('');
@@ -16,6 +21,33 @@ const Modal = ({ isOpen, onClose, reservationDetails }) => {
 
   if (!isOpen) return null;
 
+  // Crear una constante para el contenido de texto plano
+  const reservationSummary = `
+    Reservation Summary
+    ----------------------------------------------------------------------------
+    
+    User Details
+    ----------------------------------------------------------------------------
+    Guest Email: ${userEmail}
+    
+    Hotel Details
+    ----------------------------------------------------------------------------
+    Hotel: ${reservationDetails.hotel?.name}
+    Cost per Night: $${reservationDetails.hotel?.room_cost_per_night}
+    
+    Reservation Details
+    ----------------------------------------------------------------------------
+    Reservation Number: ${reservationDetails.reservationNumber}
+    Check-In Date: ${reservationDetails.checkInDate}
+    Check-Out Date: ${reservationDetails.checkOutDate}
+    Adults: ${reservationDetails.adults}
+    Children: ${reservationDetails.children}
+    Total Nights: ${reservationDetails.totalNights}
+    Total Room Cost: $${reservationDetails.totalRoomCost}
+    --------------------------------------
+    Final Total Cost: $${reservationDetails.finalTotalCost}
+  `;
+
   return (
     <div className="modal-overlay">
       <div className="modal-content">
@@ -23,13 +55,13 @@ const Modal = ({ isOpen, onClose, reservationDetails }) => {
         
         <div className="modal-section">
           <h3>User Details</h3>
-          <p>Guest Email: <strong>{userEmail}</strong></p>
+          <p>Email: <strong>{userEmail}</strong></p>
         </div>
         
         <div className="modal-section">
           <h3>Hotel Details</h3>
-          <p>Hotel: <strong>{reservationDetails.hotel?.name}</strong></p>
-          <p>Cost per Night: <strong>${reservationDetails.hotel?.room_cost_per_night}</strong></p>
+          <p>Hotel: {reservationDetails.hotel?.name}</p>
+          <p>Cost per Night: ${reservationDetails.hotel?.room_cost_per_night}</p>
         </div>
         
         <div className="modal-section">
@@ -40,10 +72,17 @@ const Modal = ({ isOpen, onClose, reservationDetails }) => {
           <p>Adults: <strong>{reservationDetails.adults}</strong></p>
           <p>Children: <strong>{reservationDetails.children}</strong></p>
           <p>Total Nights: <strong>{reservationDetails.totalNights}</strong></p>
+          <p>Total Room Cost: <strong>${reservationDetails.totalRoomCost}</strong></p>
           <p>Final Total Cost: <strong>${reservationDetails.finalTotalCost}</strong></p>
         </div>
         
-        <button className="pay-button">Pay</button>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm 
+            description={reservationSummary} 
+            amount={reservationDetails.finalTotalCost}             
+            />
+        </Elements>
+        
         <button className="close-button" onClick={onClose}>X</button>
       </div>
     </div>
